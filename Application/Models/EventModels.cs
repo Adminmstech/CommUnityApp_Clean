28ae31405ea24f282d227e92cbeb3220c6dfc267
Jahnavi Reddy <janu.poreddy@gmail.com>
2026-03-21 12:56:49 +0530

    Added volunteer status

--- Application/Models/EventModels.cs
+++ Application/Models/EventModels.cs
@@ -9,6 +9,7 @@ namespace CommUnityApp.ApplicationCore.Models
         public long CategoryId { get; set; }
 
         public string EventName { get; set; }
+
         public string Description { get; set; }
         public string Location { get; set; }
 
